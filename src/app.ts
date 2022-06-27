0c7b3eaafcb8bf991459346e7751a441e1059be1
Ashisz <acabretpaz@gmail.com>
2022-06-27 16:03:30 -0400

    se agrega healthcheck y fallback para ruta inexistentes

--- src/app.ts
+++ src/app.ts
@@ -7,4 +7,12 @@ app.use(express.json());
 
 arrayDummyRoutes(app);
 
+app.get("/", (req, res) => res.status(200).json({ message: "api funcionando correctamente" }));
+
+app.use((req, res, next) => {
+  const newError = new Error("no se encuentra la ruta");
+
+  res.status(404).json({ message: newError.message });
+});
+
 export default app;
