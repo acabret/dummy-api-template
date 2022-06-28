7fd85fe5473db2f07692caad61c7cf8b4864b6f6
Ashisz <acabretpaz@gmail.com>
2022-06-27 21:24:00 -0400

    se agregó checkeo de env

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,5 @@
-require("dotenv").config();
+if(process.env.NODE_ENV === "dev")
+    require("dotenv").config();
 
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 3000;
 
